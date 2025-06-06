508ee002b21666bcf1c2a33e831c2464a0ce9d0d
aisharay <aisharay@microsoft.com>
2025-06-06 14:44:30 +0530

    logic update

--- src/models/connectionRequest.js
+++ src/models/connectionRequest.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const ConnectionRequestSchema = new mongoose.Schema({
     sender: {
         type: mongoose.Schema.Types.ObjectId,
-        ref: 'User',
+        ref: 'User', //creates a reference to the User model
         required: true
     },
     recipient: {
@@ -20,6 +20,14 @@ const ConnectionRequestSchema = new mongoose.Schema({
         type: Date,
         default: Date.now
     }
+});
+
+//pre save middleware, this will be checked before adding new entry to db
+ConnectionRequestSchema.pre('save', function (next) {
+    if (this.sender === this.recipient) {
+        return next(new Error('You cannot send a connection request to yourself'));
+    }
+    next();
 });
 
 const ConnectionRequest = mongoose.model('ConnectionRequest', ConnectionRequestSchema);
--- src/routes/connectionRequest.js
+++ src/routes/connectionRequest.js
@@ -6,64 +6,58 @@ const { userAuth } = require('../middlewares/auth');
 const User = require('../models/user'); 
 const ConnectionRequest = require('../models/connectionRequest');
 
-connectionRequestRouter.post('/sendConnectionRequest', userAuth, async (req, res) => {
-    const user = req.user;
-    const { recipientId } = req.body;
-
-    // Validate recipientId
-    if (!recipientId) return res.status(400).send('Recipient ID is required');
-
-    // Find recipient user
-    const recipient = await User.findById(recipientId);
-    if (!recipient) return res.status(404).send('User not found');
-
-    // Send connection request logic here
-    const connectionRequest = new ConnectionRequest({ sender: user._id, recipient: recipientId , status: 'pending' });
-    connectionRequest.save();
-
-    res.send('Connection request sent');
+connectionRequestRouter.post('/request/send/interested/:userID', userAuth, async (req, res) => {
+    try {
+        const user = req.user;
+        const { senderId } = req.body;
+        if (!senderId) return res.status(400).send('Sender ID is required');
+        const sender = await User.findById(senderId);
+        if (!sender) return res.status(404).send('User not found');
+        const connectionRequest = await ConnectionRequest.findOne({ sender: senderId, recipient: user._id });
+        if (!connectionRequest) return res.status(404).send('Connection request not found');
+        connectionRequest.status = 'accepted';
+        await connectionRequest.save();
+        res.send('Connection request accepted');
+    } catch (err) {
+        res.status(500).send('Server error');
+    }
 });
 
-connectionRequestRouter.post('/request/send/interested/:userID', userAuth, async (req, res) => {
-    const user = req.user;
-    const { senderId } = req.body;
-
-    // Validate senderId
-    if (!senderId) return res.status(400).send('Sender ID is required');
-
-    // Find sender user
-    const sender = await User.findById(senderId);
-    if (!sender) return res.status(404).send('User not found');
-
-    // Accept connection request logic here
-    const connectionRequest = await ConnectionRequest.findOne({ sender: senderId, recipient: user._id });
-    if (!connectionRequest) return res.status(404).send('Connection request not found');
-
-    connectionRequest.status = 'accepted';
-    connectionRequest.save();
-
-    res.send('Connection request accepted');
-}); 
-
 connectionRequestRouter.post('/request/send/ignored/:userID', userAuth, async (req, res) => {
-    const user = req.user;
-    const { senderId } = req.body;
+    try {
+        const user = req.user;
+        const { senderId } = req.body;
+        if (!senderId) return res.status(400).send('Sender ID is required');
+        const sender = await User.findById(senderId);
+        if (!sender) return res.status(404).send('User not found');
+        const connectionRequest = await ConnectionRequest.findOne({ sender: senderId, recipient: user._id });
+        if (!connectionRequest) return res.status(404).send('Connection request not found');
+        connectionRequest.status = 'declined';
+        await connectionRequest.save();
+        res.send('Connection request declined');
+    } catch (err) {
+        res.status(500).send('Server error');
+    }
+});
 
-    // Validate senderId
-    if (!senderId) return res.status(400).send('Sender ID is required');
+connectionRequestRouter.get('/request/received', userAuth, async (req, res) => {
+    try {
+        const user = req.user;
+        const receivedRequests = await ConnectionRequest.find({ recipient: user._id });
+        res.send(receivedRequests);
+    } catch (err) {
+        res.status(500).send('Server error');
+    }
+});
 
-    // Find sender user
-    const sender = await User.findById(senderId);
-    if (!sender) return res.status(404).send('User not found');
-
-    // Decline connection request logic here
-    const connectionRequest = await ConnectionRequest.findOne({ sender: senderId, recipient: user._id });
-    if (!connectionRequest) return res.status(404).send('Connection request not found');
-
-    connectionRequest.status = 'declined';
-    connectionRequest.save();
-
-    res.send('Connection request declined');
+connectionRequestRouter.get('/request/sent', userAuth, async (req, res) => {
+    try {
+        const user = req.user;
+        const sentRequests = await ConnectionRequest.find({ sender: user._id });
+        res.send(sentRequests);
+    } catch (err) {
+        res.status(500).send('Server error');
+    }
 });
 
 module.exports = connectionRequestRouter;
